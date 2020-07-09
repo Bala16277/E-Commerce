5c55c281154a1bed73be38f7b9347d13acfe07ae
balasureshkrishnak <balasureshkrishna.kodavali@gmail.com>
2020-07-09 20:32:34 +0530

    test cases added ;

--- src/test/java/com/hcl/ecommerce/service/OrderServiceImplTest.java
+++ src/test/java/com/hcl/ecommerce/service/OrderServiceImplTest.java
@@ -1,5 +1,8 @@
 package com.hcl.ecommerce.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jboss.logging.Logger;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,6 +12,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import com.hcl.ecommerce.dto.OrderDto;
 import com.hcl.ecommerce.dto.OrderRequestDto;
 import com.hcl.ecommerce.dto.OrderResponseDto;
 import com.hcl.ecommerce.entity.Category;
@@ -86,6 +90,73 @@ public class OrderServiceImplTest {
 		}
 	}
 	
+	
+	@Test
+	public void testGetOrdersByUserId() {
+		
+		try {
+			
+			User user = new User();
+			user.setUserId(1);
+			user.setUserName("bala");
+			user.setAddress("bangalore");
+			user.setEmail("bala@gmail.com");
+			user.setPassword("bala");
+			user.setPhone("989898");
+			
+			Mockito.when(userRepository.findByUserId(1)).thenReturn(user);
+			
+			Category category = new Category();
+			category.setCategoryId(1);
+			category.setCategoryName("electronics");
+			
+			Category category1 = new Category();
+			category1.setCategoryId(2);
+			category1.setCategoryName("clothes");
+			
+			
+			Product product = new Product();
+			product.setProductId(20);
+			product.setProductDesc("pc");
+			product.setProductName("dell");
+			product.setProductPrice(1243.00);
+			product.setCategory(category);
+			
+			Product product1 = new Product();
+			product1.setProductId(18);
+			product1.setProductDesc("pant");
+			product1.setProductName("pant");
+			product1.setProductPrice(550.00);
+			product1.setCategory(category1);
+			
+			
+			Order order = new Order();
+			order.setOrderId(7);
+			order.setProduct(product);
+			order.setUser(user);
+			order.setTotalPrice(12430.00);
+			
+			Order order1 = new Order();
+			order1.setOrderId(1);
+			order1.setProduct(product1);
+			order1.setUser(user);
+			order1.setTotalPrice(750.00);
+			
+			List<Order> orders = new ArrayList<>();
+			orders.add(order);
+			orders.add(order1);
+			
+			List<OrderDto> orderDtos = orderServiceImpl.getOrdersByUserId(1);
+			Assert.assertNotNull(orderDtos);
+			Assert.assertEquals(orders.get(0).getProduct().getProductId(), orderDtos.get(0).getUserId());
+			
+			
+		}
+		catch(Exception e) {
+			
+		}
+		
+	}
 	
 
 }
